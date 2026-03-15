6a5147fac4a0432cc8ab11eb78f24c230cbb7896
Alfakynz <alfakynz.nyxaeris@gmail.com>
2026-03-15 23:06:10 +0100

    Add support for the game difficulty

--- src/main/java/com/alfakynz/defaultgamemode/config/Config.java
+++ src/main/java/com/alfakynz/defaultgamemode/config/Config.java
@@ -1,6 +1,7 @@
 package com.alfakynz.defaultgamemode.config;
 
 import net.minecraft.client.gui.screens.worldselection.WorldCreationUiState.SelectedGameMode;
+import net.minecraft.world.Difficulty;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,6 +17,7 @@ public class Config {
     private static final Path CONFIG_PATH = Path.of("config", "default-gamemode.txt");
 
     public static SelectedGameMode GAMEMODE = SelectedGameMode.CREATIVE;
+    public static Difficulty DIFFICULTY = Difficulty.NORMAL;
 
     public static void load() {
         if (!Files.exists(CONFIG_PATH)) {
@@ -40,6 +42,17 @@ public class Config {
                         default -> GAMEMODE = SelectedGameMode.CREATIVE;
                     }
                 }
+
+                if (line.startsWith("difficulty=")) {
+                    String value = line.substring("difficulty=".length()).toLowerCase();
+
+                    switch (value) {
+                        case "peaceful" -> DIFFICULTY = Difficulty.PEACEFUL;
+                        case "easy" -> DIFFICULTY = Difficulty.EASY;
+                        case "hard" -> DIFFICULTY = Difficulty.HARD;
+                        default -> DIFFICULTY = Difficulty.NORMAL;
+                    }
+                }
             }
         } catch (IOException e) {
             LOGGER.error("Failed to load Default Gamemode configuration.", e);
@@ -48,7 +61,9 @@ public class Config {
 
     public static void save() {
         try (Writer writer = new FileWriter(CONFIG_PATH.toString())) {
-            writer.write("# Choose your default gamemode for new worlds.\n");
+            writer.write("# Choose your default gamemode and difficulty for new worlds.\n");
+
+            // Gamemode
             writer.write("# Options: survival, creative, hardcore, spectator\n");
             String gamemodeString;
             switch (GAMEMODE) {
@@ -57,7 +72,18 @@ public class Config {
                 case DEBUG -> gamemodeString = "spectator";
                 default -> gamemodeString = "creative";
             }
-            writer.write("gamemode=" + gamemodeString + "\n");
+            writer.write("gamemode=" + gamemodeString + "\n\n");
+
+            // Difficulty
+            writer.write("# Options: peaceful, easy, normal, hard\n");
+            String difficultyString;
+            switch (DIFFICULTY) {
+                case PEACEFUL -> difficultyString = "peaceful";
+                case EASY -> difficultyString = "easy";
+                case HARD -> difficultyString = "hard";
+                default -> difficultyString = "normal";
+            }
+            writer.write("difficulty=" + difficultyString);
         } catch (IOException e) {
             LOGGER.error("Failed to save Default Gamemode configuration.", e);
         }
@@ -71,6 +97,9 @@ public class Config {
                          # Choose your default gamemode for new worlds.
                          # Options: survival, creative, hardcore, spectator
                          gamemode=creative
+                         
+                         # Options: peaceful, easy, normal, hard
+                         difficulty=normal
                          """);
         } catch (IOException e) {
             LOGGER.error("Failed to create Default Gamemode configuration file.", e);
---
+++ src/main/java/com/alfakynz/defaultgamemode/mixin/GameDifficultyMixin.java
@@ -0,0 +1,29 @@
+package com.alfakynz.defaultgamemode.mixin;
+
+import com.alfakynz.defaultgamemode.config.Config;
+import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
+import net.minecraft.client.gui.screens.worldselection.WorldCreationUiState;
+import net.minecraft.world.Difficulty;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CreateWorldScreen.class)
+public abstract class GameDifficultyMixin {
+
+    @Final
+    @Shadow
+    WorldCreationUiState uiState;
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void setDefaultGamemodeCreative(CallbackInfo ci) {
+        this.uiState.setDifficulty(
+                Difficulty.valueOf(
+                        Config.DIFFICULTY.name()
+                )
+        );
+    }
+}
